8be4d0b9359f982c3362a97d14bfa2011c23acc3
ksw0102 <hanarudy467@gmail.com>
2024-01-10 10:18:15 +0900

    240110

--- src/main/java/com/dw/Monaca/controller/MessageController.java
+++ src/main/java/com/dw/Monaca/controller/MessageController.java
@@ -4,13 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.dw.Monaca.dto.ResponseDto;
 import com.dw.Monaca.model.Message;
 import com.dw.Monaca.service.impl.MessageServiceImpl;
 
+@RestController
+@CrossOrigin(origins = "http://localhost:3000", methods = { RequestMethod.GET, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.POST })
 public class MessageController {
 
 	 private final MessageServiceImpl messageService;
@@ -25,8 +30,7 @@ public class MessageController {
 	@PreAuthorize("hasAnyRole('USER','ADMIN')")
 	public ResponseEntity<ResponseDto<Message>> deleteMessage(@PathVariable Long id) {
 		
-		return new ResponseEntity<>(
-				messageService.deleteMessage(id),
+		return new ResponseEntity<>(messageService.deleteMessage(id),
 				HttpStatus.OK);
 	}
 	
